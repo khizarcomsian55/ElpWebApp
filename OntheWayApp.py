1f3357c76b50819f39a1cc03e07a47f0dd278552
Khizar Raza <khizar.raza7@gmail.com>
2024-12-02 09:28:22 +0500

    Update OntheWayApp.py

--- OntheWayApp.py
+++ OntheWayApp.py
@@ -71,7 +71,7 @@ st.write("This Application is Designed for Evaluation of ELP On The Way Vehicles
 
 # Sidebar for database connection parameters
 st.sidebar.header("Database Connection")
-server = st.sidebar.text_input("Server", placeholder="10.10.0.51:1433")
+server = st.sidebar.text_input("Server", placeholder="119.156.28.26:1433")
 database = st.sidebar.text_input("Database", placeholder="ElpWebData")
 
 # Fetch data function
